c7aeeb3e2b4a18be84e9c959a605abda160d9e65
akishajae <akishajae.angeles@gmail.com>
2024-12-03 00:01:26 +0100

    fixed errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListAllNursesComponent } from './list-all-nurses/list-all-nurses.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListAllNursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
